4a93be089667faae6ac3bef724405f836b238482
JonasGarciaGit <=>
2022-04-06 19:50:37 -0300

    eureka-server created

---
+++ tcc-eureka-server/src/main/java/com/tcc/eurekaserver/TccEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccEurekaServerApplication.class, args);
+	}
+
+}
