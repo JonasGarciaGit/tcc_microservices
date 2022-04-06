2687b277142461a5ea32836ff9c35dde4fe7b5b5
JonasGarciaGit <=>
2022-04-06 20:08:50 -0300

    api gateway created

---
+++ tcc-api-gateway-zuul/src/main/java/com/tcc/apigatewayzuul/TccApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccApiGatewayZuulApplication.class, args);
+	}
+
+}
